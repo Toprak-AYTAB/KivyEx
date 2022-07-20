b7f64fd4cac8b0715754daf0bc03c62db0f778f3
Toprak-AYTAB <67775789+Toprak-AYTAB@users.noreply.github.com>
2022-07-20 13:06:30 +0300

    Add files via upload

---
+++ üst f4.py
@@ -0,0 +1,14 @@
+
+import kivy
+kivy.require('2.1.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class Üstf4(App):
+    def build(self):
+        return Label(text="Kiviiiiiiiiğğ")
+
+if __name__ == '__main__':
+    Üstf4().run()
